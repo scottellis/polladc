bdc13e920dda3daf4784b30653c2f066f805675a
Scott Ellis <scott@pansenti.com>
2012-10-19 08:51:09 -0400

    Do some simple update rate adjustment based on scan rate

--- polladc.c
+++ polladc.c
@@ -20,7 +20,6 @@ FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER
 DEALINGS IN THE SOFTWARE.
 */
 
-
 #include <stdio.h>
 #include <string.h>
 #include <stdlib.h>
@@ -166,7 +165,7 @@ void show_elapsed(struct timeval *start, struct timeval *end, int count)
 
 int loop(int delay_ms, int *list)
 {
-    int fd, count, i;
+    int fd, count, i, update;
     int val[NUM_ADC];
 
     count = 0;
@@ -201,8 +200,24 @@ int loop(int delay_ms, int *list)
                 break;
         }
 
+        update = 0;
+
         // don't spend too much time updating display
-        if ((count & 0x001f) == 0) {
+        if (delay_ms < 10) {
+            // every 32nd read
+            if ((count & 0x1f) == 0) 
+                update = 1;
+        }
+        else if (delay_ms < 50) {
+            // every 8th read
+            if ((count & 0x07) == 0)
+                update = 1;
+        }
+        else {
+            update = 1;
+        }
+
+        if (update) {
             fprintf(stdout, "\rRead %8d: ", count);
 
             for (i = 0; i < NUM_ADC; i++) {            
@@ -240,8 +255,8 @@ int read_adc(int fd)
 
 int open_adc(int adc)
 {
-   char path[128];
-   sprintf(path, "%sin%d_input", hwmonsyspath, adc);
+    char path[128];
+    sprintf(path, "%sin%d_input", hwmonsyspath, adc);
 
     int fd = open(path, O_RDONLY);
 
